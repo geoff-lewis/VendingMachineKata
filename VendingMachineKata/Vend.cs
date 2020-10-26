06e3001eb56f37da1e15bcbdc30176b3d00af8b8
Geoff.Lewis <Geoff.Lewis@atmosi.com>
2020-10-26 16:33:17 +0000

    First commmit with basic vending machine using a procedural pattern

---
+++ VendingMachineKata/Vend.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachineKata
+{
+    public class Vend
+    {
+        public VendResult VendResult { get; }
+        public double Change { get; }
+        
+        private Vend(VendResult vendResult, double change)
+        {
+            VendResult = vendResult;
+            Change = change;
+        }
+
+        public static Vend UnableToVend => new Vend(VendResult.UnableToVend,0);
+
+        public static Vend SuccessfulVend(double change) => new Vend(VendResult.SuccessfulVend,change);
+
+    }
+}
